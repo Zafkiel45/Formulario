6e2cacdba4a091ac13e27e8ab4bc2ab380febff6
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-03-27 18:20:01 -0300

    remake

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -4,30 +4,54 @@ const email = document.querySelector('#email');
 const more_info = document.querySelector('#sobre');
 const termo_uso = document.querySelector('#checked');
 const input = document.querySelectorAll('.input');
+const button = document.querySelector('#button');
+const form = document.querySelector('#form');
 
 
 input.forEach(function(element){
     element.addEventListener('input', function(){
         // input name
-        if(input[0].value.length < 4){
+        if(input[0].value.length < 4 || !input[0].checkValidity()){
             erro(0);
+            input[0].classList.add('erro')
         } else {
             valid(0);
+            input[0].classList.remove('erro')
+        }
+        // input email
+        if(!input[1].checkValidity()){
+            erro(1)
+        } else {
+            valid(1)
         }
         // input textarea
-        if(input[1].value.length < 4){
-            erro(1);
+        if(input[2].value.length < 4){
+            erro(2);
         }else {
-            valid(1);
+            valid(2);
+        }
+        // input checkbox
+        if(!input[3].checked){
+            erro(3);
+        } else {
+            valid(3);
         }
     })
 });
-
+button.addEventListener('click', function(){
+    for(let verifique = 0; verifique < input.length; verifique++){
+        if(input[verifique].classList.contains('erro')){
+            erro(verifique);
+        }
+    }
+    if(!input[verifique].classList.contains('erro')){
+        form.submit()
+    }
+})
 function erro(idx){
     text_error[idx].style.display = 'block'
 } 
-
-function valid(){
+function valid(idx){
     text_error[idx].style.display = 'none'
 }
 
---
+++ JavaScript/sidebar.js
@@ -0,0 +1,21 @@
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const sidebar = document.querySelector('#sidebar');
+const body = document.querySelector('#corpo');
+const section = document.querySelectorAll('.seção');
+
+next.addEventListener('click', function(){
+    sidebar.style.left = '0'
+    body.style.overflow = 'hidden'
+    section[0].style.filter = 'blur(4px)'
+})
+prev.addEventListener('click', function(){
+    sidebar.style.left = '-100%'
+    body.style.overflow = ''
+    section[0].style.filter = ''
+})
+section[0].addEventListener('click', function(){
+    sidebar.style.left = '-100%'
+    section[0].style.filter = ''
+    body.style.overflow = ''
+})
